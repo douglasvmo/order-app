d6e4279f329b57d6167069e3e445b19a971934e5
Douglas <douglasvmo@icloud.com>
2026-04-18 13:39:19 -0300

    ui: update card shape

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.simpleorderapplication"
         minSdk = 29
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
